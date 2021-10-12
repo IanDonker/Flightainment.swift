f576514af41b7fcd8cea35d1499960b845669a6d
Ian Donker <iandonker7@gmail.com>
2021-10-12 19:41:16 +0200

    Initial Commit

---
+++ Flightainment.swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flightainment.swift
+//
+//  Created by Ian Donker on 12/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flightainment.swift/Flightainment_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flightainment_swiftApp.swift
+//  Flightainment.swift
+//
+//  Created by Ian Donker on 12/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Flightainment_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
